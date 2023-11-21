8d3090c4c9a65e07edae7b600d6c272c87c649fd
trang2003xinh <151649105+trang2003xinh@users.noreply.github.com>
2023-11-22 00:16:45 +0700

    Update Item.jsx

--- Components/Item/Item.jsx
+++ Components/Item/Item.jsx
@@ -1,9 +1,19 @@
-import './Item.css'
-function Item(){
-    return(
-        <div className="item">
-            
+import React from 'react' 
+import '.Item.css'
+
+const Item = (props) =>{
+    return (
+        <div className = 'item'>
+            <Link to={'/product/${props.id}'}><img src={props.image} alt="" /></Link>
+            <p>{props.name}</p>
+            <div className="item-prices">
+                <div className="item-price-new">
+                    ${props.new_price}
+                </div>
+                <div className="item-price-old">
+                    ${props.old_price}
+                </div>
+            </div>
         </div>
-    );
+    )
 }
-export default Item
