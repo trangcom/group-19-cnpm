6cb0020f85dc07b06b5ddd1213c2566306a6282e
trang2003xinh <151649105+trang2003xinh@users.noreply.github.com>
2023-11-21 23:07:08 +0700

    Update Cart.jsx

--- Pages/Cart.jsx
+++ Pages/Cart.jsx
@@ -1,8 +1,11 @@
 
-function Cart(){
+// import React from "react"; 6.9k (gzipped: 2.7k)
+import CartItems from "../Components/CartItems/CartItems";
+
+const Cart = () => {
     return(
         <div>
-
+            <CartItems/>
         </div>
     );
 }
