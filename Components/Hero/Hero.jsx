98655a2fa7db7fe0a2c75b5ab42f7904bc9230a7
Junnie26 <151734241+Junnie26@users.noreply.github.com>
2023-11-22 21:59:20 +0700

    Update Hero.jsx

--- Components/Hero/Hero.jsx
+++ Components/Hero/Hero.jsx
@@ -1,16 +1,33 @@
+import React from 'react' 6.9k (gzipped: 2.7k)
 import './Hero.css'
+import hand_icon from '../Asset/hand_icon.png'
+import arrow_icon from '../Asser/arrow.png'
+import hero_image from '../Asser/hero_image.png'
 
-function Hero(){
+const Hero = () => {
     return (
-        <div className="hero">
-            <div className="hero-left">
-                <h2></h2>
-
+        <div className= 'hero'>
+          <div className=  "hero-left">
+            <h2>NEW ARRIVALS ONLY</h2>
+            <div>
+            <div className= "hero-hand-icon">
+                <p>new</p>
+                <img src={hand_icon} alt="" />
+            </div>  
+            <p>collection</p>
+            <p>for everyone</p>
             </div>
-            <div className="hero-right">
-
-            </div>
+                <div className= "hero-latest-btn">
+                    <div>latest collection</div>
+                    <img src={arrow_icon} alt="" />
+                </div>
+          </div>
+            
+          <div className= "hero-right">
+            <img src={hero_image} alt="" />
+          </div>
         </div>
-    );
+    )
 }
+
 export default Hero
