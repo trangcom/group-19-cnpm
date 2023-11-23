5265951b7d933ffe4cdd54d92bfc4a941f249e8a
Junnie26 <151734241+Junnie26@users.noreply.github.com>
2023-11-23 19:19:38 +0700

    Update Item.jsx

--- Components/Item/Item.jsx
+++ Components/Item/Item.jsx
@@ -4,7 +4,7 @@ import '.Item.css'
 const Item = (props) =>{
     return (
         <div className = 'item'>
-            <Link to={'/product/${props.id}'}><img src={props.image} alt="" /></Link>
+            <Link to={'/product/${props.id}'}><img onClick={scrollTo(0,0)}/>
             <p>{props.name}</p>
             <div className="item-prices">
                 <div className="item-price-new">
