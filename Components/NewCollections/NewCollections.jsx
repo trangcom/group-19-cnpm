7c8f1c407aff53b4ba7c5416d420557325b1413d
Junnie26 <151734241+Junnie26@users.noreply.github.com>
2023-11-23 19:24:13 +0700

    Update NewCollections.jsx

--- Components/NewCollections/NewCollections.jsx
+++ Components/NewCollections/NewCollections.jsx
@@ -9,7 +9,7 @@ const Newcollection = () => {
           <hr />
           <div className = "collections">
             {new_collection.map((item,i) => {
-                return 
+                return <Item key = {i} id = {item.id} name = {item.name} image = {item.image} new_price = {item.new_price} old_price = {item.old_price}/>
             })}
           </div>
 
