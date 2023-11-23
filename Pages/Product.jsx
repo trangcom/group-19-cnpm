81f863212923406b1db74cc3c2a328592f25105d
trangcom <147507856+trangcom@users.noreply.github.com>
2023-11-23 22:17:20 +0700

    Update Product.jsx

--- Pages/Product.jsx
+++ Pages/Product.jsx
@@ -1,9 +1,14 @@
-
-function Product(){
-    return(
+import React from 'react' 
+import Breadcrum from './Breadcrum/Breadcrum';
+const Product = () => {
+   const {all_product}= useContext(ShopContext);
+   const{productId}= useParams();
+   const product = all_product.find((e)=> e.id === Number(productId));
+    return (
         <div>
-
+            <Breadcrum product={product}/>
         </div>
-    );
+    )
 }
+
 export default Product
