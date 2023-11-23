b496d067ffadcec256c41b150ee78002b19d813c
Junnie26 <151734241+Junnie26@users.noreply.github.com>
2023-11-23 16:14:59 +0700

    Update ShopCategory.jsx

--- Pages/ShopCategory.jsx
+++ Pages/ShopCategory.jsx
@@ -1,5 +1,26 @@
+import React from 'react' 6.9k (gzipped: 2.7k)
+import './CSS/ShopCategory.css'
+import { ShopContext } from '../Context/ShopContext'
+import dropdown_icon from '../component/ Assets/dropdown_icon.png'
+const ShopCategory = (props) => { 
+     const {all_product} = useContext(Shopcontext);
 
+    return (
+        <div className= 'shop-category'>
+            <img src={props.banner} alt="" />
+            <div className="shopcategory-indexsort">
+                <p>
+                    <span>showing 1-12</span> out of 36 products
+                </p>
+                <div className="shopcategory-sort" > 
+                    sort by <img src="" alt="" />
+                </div>
+            </div>
+        </div>
+  )
+}
 
+export default ShopCategory
 function ShopCategory(){
     return(
         <div>
