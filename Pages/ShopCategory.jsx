9fb0be1ffba8d7d4e406343bd06263f842106187
Junnie26 <151734241+Junnie26@users.noreply.github.com>
2023-11-23 18:54:25 +0700

    Update ShopCategory.jsx

--- Pages/ShopCategory.jsx
+++ Pages/ShopCategory.jsx
@@ -2,6 +2,7 @@ import React from 'react' 6.9k (gzipped: 2.7k)
 import './CSS/ShopCategory.css'
 import { ShopContext } from '../Context/ShopContext'
 import dropdown_icon from '../component/ Assets/dropdown_icon.png'
+import Item from '../components/Item/Item'
 const ShopCategory = (props) => { 
      const {all_product} = useContext(Shopcontext);
 
@@ -13,9 +14,18 @@ const ShopCategory = (props) => {
                     <span>showing 1-12</span> out of 36 products
                 </p>
                 <div className="shopcategory-sort" > 
-                    sort by <img src="" alt="" />
+                    sort by <img src="dropdown_icon" alt="" />
                 </div>
             </div>
+             <div className="shopcategory-products>
+             {all_product.map((item,i) => {
+             if (props.category == item.category) {
+                  return <Item key = {i} id = {item.id} name = {item.name} image = {item.image} new_price = {item.new_price} old_price = {item.old_price}/>
+             }
+             else { 
+                  return null;
+        })}
+        </div>
         </div>
   )
 }
